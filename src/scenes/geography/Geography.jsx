b0f03b07cf9c1ed034ceef38e3577c320b1cc623
Amal-Elafifi <160831057+Amal-Elafifi@users.noreply.github.com>
2025-02-18 00:30:01 -1100

    charts-created

--- src/scenes/geography/Geography.jsx
+++ src/scenes/geography/Geography.jsx
@@ -1,8 +1,18 @@
+import { Box, useTheme } from "@mui/material"; 
+import GeographicChart from "../../components/GeographicChart";
+import Header from "../../components/Header";
+import { tokens } from "../global";
+
 const Geography= () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <div>
-      Geography
-    </div>
+    <Box m="20px">
+      <Header  title="Geography Chart" subtitle="Simple Geography Chart"/>
+      <Box height="75vh" border={`1px solid ${colors.grey[100]}` }borderRadius="4px">
+        <GeographicChart/>
+      </Box>
+    </Box>
   )
 }
 
